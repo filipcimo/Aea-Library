a19c9a0f26470cfe1eadf641a8c72344d5da17c1
Filip Vojtko <66070449+filipcimo@users.noreply.github.com>
2021-05-19 18:40:42 +0200

    Create aea.h

---
+++ aea/aea.h
@@ -0,0 +1,13 @@
+#ifndef AEA_H
+#define AEA_H
+
+
+#include "BasicContainer.h"
+#include "var.h"
+#include "sentence.h"
+#include "DArray.h"
+#include "Pointer.h"
+#include "Functions.h"
+
+
+#endif  // AEA_H
